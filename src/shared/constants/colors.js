daeac6994272c6d90ab5b6fef8eef05a0de53fb7
Son Nguyen <nguyentranson2k3@gmail.com>
2025-03-31 10:59:34 +0700

    feat: [SCRUM-119,120] add utils, create constant and  set css global

---
+++ src/shared/constants/colors.js
@@ -0,0 +1,31 @@
+export const COLORS = {
+  // Status colors
+  STATUS_COMPLETED_BG: '#E5FAF5',
+  STATUS_COMPLETED_TEXT: '#00B087',
+  STATUS_IN_PROGRESS_BG: '#E5F6FF',
+  STATUS_IN_PROGRESS_TEXT: '#0091FF',
+  STATUS_PENDING_BG: '#FFF5E5',
+  STATUS_PENDING_TEXT: '#FF9500',
+  STATUS_DEFAULT_BG: '#F5F5F5',
+  STATUS_DEFAULT_TEXT: '#666666',
+
+  // Common colors
+  PRIMARY: '#1890ff',
+  SUCCESS: '#52c41a',
+  WARNING: '#faad14',
+  ERROR: '#f5222d',
+
+  // Text colors
+  TEXT_PRIMARY: '#262626',
+  TEXT_SECONDARY: '#595959',
+  TEXT_DISABLED: '#bfbfbf',
+
+  // Background colors
+  BG_DEFAULT: '#ffffff',
+  BG_GRAY_LIGHT: '#f5f5f5',
+  BG_GRAY: '#e8e8e8',
+
+  // Border colors
+  BORDER: '#d9d9d9',
+  BORDER_LIGHT: '#f0f0f0'
+}
