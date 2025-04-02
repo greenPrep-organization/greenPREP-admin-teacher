f6f21922144292af9320da1cb8be356c856c6f9f
punhnahk <pun.hnahk@gmail.com>
2025-04-02 17:51:24 +0700

    redo

--- src/features/profile/api/profileAPI.js
+++ src/features/profile/api/profileAPI.js
@@ -1,13 +1,13 @@
 import { useMutation, useQuery } from '@tanstack/react-query'
 import axios from 'axios'
 
-const API_BASE_URL = 'https://dev-api-greenprep.onrender.com'
+const API_BASE_URL = import.meta.env.VITE_BASE_URL
 const accessToken = localStorage.getItem('access_token')
 
 const fetchUserProfile = async userId => {
   try {
     console.log(`Fetching user profile for userId: ${userId}`)
-    const { data } = await axios.get(`${API_BASE_URL}/api/users/${userId}`, {
+    const { data } = await axios.get(`${API_BASE_URL}/users/${userId}`, {
       headers: { Authorization: `Bearer ${accessToken}` }
     })
     return data
