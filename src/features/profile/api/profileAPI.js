d67e4962f995bf81218118792235e9077e61717c
punhnahk <pun.hnahk@gmail.com>
2025-04-02 16:56:01 +0700

    refactor: Replace axios with axiosInstance for API calls and remove access token dependency

--- src/features/profile/api/profileAPI.js
+++ src/features/profile/api/profileAPI.js
@@ -1,15 +1,12 @@
+import axiosInstance from '@shared/config/axios'
 import { useMutation, useQuery } from '@tanstack/react-query'
-import axios from 'axios'
 
 const API_BASE_URL = import.meta.env.VITE_BASE_URL
-const accessToken = localStorage.getItem('access_token')
 
 const fetchUserProfile = async userId => {
   try {
     console.log(`Fetching user profile for userId: ${userId}`)
-    const { data } = await axios.get(`${API_BASE_URL}/users/${userId}`, {
-      headers: { Authorization: `Bearer ${accessToken}` }
-    })
+    const { data } = await axiosInstance.get(`${API_BASE_URL}/users/${userId}`)
     return data
   } catch (error) {
     console.error('Error fetching user profile:', error)
@@ -19,9 +16,7 @@ const fetchUserProfile = async userId => {
 
 const updateUserProfile = async ({ userId, userData }) => {
   try {
-    const { data } = await axios.put(`${API_BASE_URL}/users/${userId}`, userData, {
-      headers: { Authorization: `Bearer ${accessToken}` }
-    })
+    const { data } = await axiosInstance.put(`${API_BASE_URL}/users/${userId}`, userData)
     return data
   } catch (error) {
     console.error('Error updating user profile:', error)
@@ -31,9 +26,7 @@ const updateUserProfile = async ({ userId, userData }) => {
 
 const changeUserPassword = async ({ userId, passwordData }) => {
   try {
-    const { data } = await axios.post(`${API_BASE_URL}/users/${userId}/change-password`, passwordData, {
-      headers: { Authorization: `Bearer ${accessToken}` }
-    })
+    const { data } = await axiosInstance.post(`${API_BASE_URL}/users/${userId}/change-password`, passwordData)
     return data
   } catch (error) {
     console.error('Error changing password:', error)
@@ -45,7 +38,7 @@ export const useUserProfile = userId => {
   return useQuery({
     queryKey: ['userProfile', userId],
     queryFn: async () => await fetchUserProfile(userId),
-    enabled: !!userId && !!accessToken
+    enabled: !!userId
   })
 }
 
