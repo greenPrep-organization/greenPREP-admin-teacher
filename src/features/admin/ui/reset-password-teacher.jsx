95d25b5864da346df6cc96f76f629759e68148e8
punhnahk <pun.hnahk@gmail.com>
2025-04-08 09:57:04 +0700

    feat: remove randomGenerationPassword

--- src/features/admin/ui/reset-password-teacher.jsx
+++ src/features/admin/ui/reset-password-teacher.jsx
@@ -6,16 +6,6 @@ const ResetPasswordModal = ({ email, isVisible, onCancel, onResetSuccess }) => {
   const [loading, setLoading] = useState(false)
   const resetPasswordMutation = useResetPassword()
 
-  // eslint-disable-next-line no-unused-vars
-  const generateRandomPassword = () => {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()'
-    let password = ''
-    for (let i = 0; i < 12; i++) {
-      password += characters.charAt(Math.floor(Math.random() * characters.length))
-    }
-    return password
-  }
-
   const handleResetPassword = async () => {
     setLoading(true)
 
@@ -40,7 +30,7 @@ const ResetPasswordModal = ({ email, isVisible, onCancel, onResetSuccess }) => {
       okText="Reset Password"
       cancelText="Cancel"
     >
-      <p>Are you sure you want to reset the password? A random password will be generated and sent to the user.</p>
+      <p>This will send a password reset email to the user. Do you want to proceed?</p>
     </Modal>
   )
 }
