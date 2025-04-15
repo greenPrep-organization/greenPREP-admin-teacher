b4c7c26225ba7fef0b87e4078fd9c2e6447619e4
punhnahk <pun.hnahk@gmail.com>
2025-04-15 09:51:20 +0700

    fix: update CompleteCount calculation to filter completed sessions instead of not started

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -55,7 +55,7 @@ const DashboardPage = () => {
   const teacherSessions = sessionsArray.filter(
     session => session?.Classes?.UserID?.trim() === auth.user?.userId?.trim()
   )
-  const CompleteCount = teacherSessions.filter(session => session.status?.toUpperCase() === 'NOT_STARTED').length
+  const CompleteCount = teacherSessions.filter(session => session.status?.toUpperCase() === 'COMPLETED').length
   const myClassCount = myClasses?.length || 0
   const [ungradedCount, setUngradedCount] = useState(0)
 
